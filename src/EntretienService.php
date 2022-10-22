1b8fc8686b72472313049b5b7b9cad9e640fc3e8
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-10-22 11:05:57 +0200

    workshop 2: solution implement planifier second scenario : green test

--- src/EntretienService.php
+++ src/EntretienService.php
@@ -17,6 +17,9 @@ class EntretienService
 
     public function planifier(Candidat $candidat, $disponibiliteCandidat, Recruteur $recruteur, $disponibiliteRecruteur)
     {
+        if ($disponibiliteCandidat != $disponibiliteRecruteur) {
+            return false;
+        }
         $this->entretienRepository->save(new Entretien($candidat->getEmail(), $recruteur->getEmail(), $disponibiliteRecruteur));
 
         $this->emailService->sendToRecruteur($recruteur->getEmail());
